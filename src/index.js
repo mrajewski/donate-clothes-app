a934933d8053a9a9205d95a830fe09f3f6d1bfca
mrajewski <mrajewski1@st.swps.edu.pl>
2019-08-14 16:00:52 +0200

    header

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+    HashRouter,
+    Route,
+    Link,
+    Switch,
+    NavLink,
+} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
